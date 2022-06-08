d943e19a74cdb7d0d966c5d61c4cb3783d0737cd
topurbancraft <auchennamagnus@gmail.com>
2022-06-08 08:43:31 -0700

    New commit

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    '''
+    Returns the number of keys in a dictionary
+    Parameters:
+    a_dictionary: Dictionary with keys
+    '''
+    m = len(a_dictionary.keys())
+    return m
