8f8a492a489b9177d33bd5b3a46b6afbf6fbb003
topurbancraft <auchennamagnus@gmail.com>
2022-06-22 02:37:15 +0100

    New commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''A module for working with squares.
+'''
+
+
+class Square:
+    '''Represents a 2D Polygon with 4 equal and perpendicular sides.
+    '''
+    def __init__(self, size=0):
+        '''Initializes a Square with a given size.
+        Args:
+            size (int): The size of the square.
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        '''Retrieves the size of this Square.
+        Returns:
+            int: The size of this Square.
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Updates the size of this Square.
+        Args:
+            value (int): The new size of this Square.
+        '''
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    def area(self):
+        '''Computes the area of this Square.
+        Returns:
+            int: The area of this Square.
+        '''
+        return self.size ** 2
+
+    def my_print(self):
+        '''Prints a 2D table of the '#' symbol with the size of this square.
+        '''
+        if self.size == 0:
+            print('')
+        else:
+            for i in range(self.size):
+                print('{}'.format('#' * self.size))
