7f81417e3e8bc82f865ec96ccfff1eceb90ed190
topurbancraft <auchennamagnus@gmail.com>
2022-06-29 19:36:42 +0100

    New commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module for working with rectangles.
+"""
+
+
+class Rectangle:
+    """
+    Represents a 2D 4 sided Polygon with 2 set of equal sides.
+    """
+    pass
