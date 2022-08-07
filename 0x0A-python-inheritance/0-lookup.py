b4cc68893cfcde880f7c343f5752ef34c7d79670
topurbancraft <auchennamagnus@gmail.com>
2022-08-07 23:17:41 +0100

    new commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''A module for inspecting an object.
+'''
+
+
+def lookup(obj):
+    '''Finds the list of available attributes and methods of an object.
+    Args:
+        obj (any): A given object.
+    Returns:
+        list: A list of available attributes and methods the object has.
+    '''
+    return dir(obj)
